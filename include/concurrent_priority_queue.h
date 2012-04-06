02cfc647689c03317481ed908f65388bda860ef0
alasdair <alasdair@003f9db4-d5f8-dca4-bccd-e9ef3f936871>
2012-04-06 22:35:20 +0000

    Initial revision of concurrent_priority_queue. Incomplete.

---
+++ include/concurrent_priority_queue.h
@@ -0,0 +1,174 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+#ifndef STD_CONCURRENT_PRIORITY_QUEUE_
+#define STD_CONCURRENT_PRIORITY_QUEUE_
+
+#include <algorithm>
+#include <functional>
+#include <string>
+#include <vector>
+
+#include <thread.h>
+
+// TODO(alasdair): Initial version being checked in without
+// locking. Make this genuinely threadsafe. Document exact details of
+// thread safety.
+//
+//
+// A queue of elements, ordered by priority. Elements within the queue
+// are compared using std::less, and an element with a high priority
+// is considered to be greater than one with a lower priority.
+//
+// An element may be removed from the head of the queue using
+// pop(). The element so removed will be the highest priority element
+// currently in the queue. (If multiple elements have the same highest
+// priority, there is no guarantee as to which element will be removed
+// first.)
+//
+// This class is thread safe. Elements may be added and removed from
+// multiple threads.
+
+template <typename T,
+          class Cont = std::vector<T>,
+          class Less = std::less<T> >
+class concurrent_priority_queue {
+ public:
+  typedef T value_type;
+  typedef T& reference;
+  typedef const T& const_reference;
+  typedef std::size_t size_type;
+
+  concurrent_priority_queue() {
+  }
+
+  // requires CopyConstructible<Cont>
+  explicit concurrent_priority_queue(const Cont& cont) : cont_(cont) {
+    make_heap();
+  }
+
+  // requires CopyConstructible<Cont> & CopyConstructible<Less>
+  concurrent_priority_queue(const Less& c, const Cont& cont)
+      : less_(c), cont_(cont) {
+    make_heap();
+  }
+
+  // requires CopyConstructible<Cont> & RangeInsertionContainer<Cont, Iter> &
+  // CopyConstructible<Less>
+  template <typename Iter>
+  concurrent_priority_queue(Iter first,
+                            Iter last,
+                            const Less& less,
+                            const Cont& cont)
+    : less_(less), cont_(cont) {
+    cont_.insert(cont_.end(), first, last);
+    make_heap();
+  }
+
+  // requires MoveConstructible<Cont> & RangeInsertionContainer<Cont, Iter>
+  template <typename Iter>
+  concurrent_priority_queue(Iter first, Iter last) : cont_(first, last) {
+    make_heap();
+  }
+
+#if 0
+  // TODO(alasdair): Jeffrey suggests that these constructors, plus the update()
+ // method, may not be necessary. (What is the use case for the
+ // assignment and copy constructor?) Verify if we need these, and if
+ // not remove them.
+
+  // requires MoveConstructible<Cont>
+  concurrent_priority_queue(const concurrent_priority_queue& other)
+     : less_(other.less_), cont_(other.cont_) {
+  }
+
+  // requires MoveAssignable<Cont>
+  concurrent_priority_queue& operator=(const concurrent_priority_queue& other) {
+    this.cont_ = other.cont_;
+    this.less_ = other.less_;
+  }
+#endif
+
+  // TODO(alasdair): verify whether or not we need this.
+  //
+  // Re-evaluates the order of elements in the queue, using a new
+  // comparator.
+  void update(const Less& less) {
+    less_ = less;
+    make_heap();
+  }
+
+  // Returns true if this queue is empty
+  bool empty() const { return cont_.empty(); }
+
+  // Returns the number of elements in the queue
+  size_type size() const { return cont_.size(); }
+
+  // Adds a new element to the queue.
+  void push(const value_type& x) {
+    cont_.push_back(x);
+    push_heap(cont_.begin(), cont_.end(), less_);
+  }
+
+#if 0
+  // TODO(alasdair): Implement when we have C++0x compatibility mode.
+  void push(value_type&& x) {
+    cont_.push_back(x);
+    push_heap(cont_.begin(), cont_.end(), less_);
+  }
+#endif
+
+#if 0
+  // TODO(alasdair): Add this when we have support for the '...' construct
+  // requires BackEmplacementContainer<Cont, Args&...>
+  template <class... Args>
+  void emplace(Args&... args);
+#endif
+
+  // requires MoveAssignable<value_type>
+  bool try_pop(value_type& out) {
+    if (cont_.empty()) {
+      return false;
+    } else {
+      pop_heap(cont_.begin(), cont_.end(), less_);
+      out = cont_.back();
+      cont_.pop_back();
+      return true;
+    }
+  }
+
+#if 0
+  // TODO(alasdair): Add this when we have the timeout type
+  // requires MoveAssignable<value_type>
+  bool wait_pop(value_type& out, timeout t);
+#endif
+
+  // Returns the element at the front of the queue. This will be the
+  // element with the highest priority. Blocks until an element is
+  // available.
+  // requires MoveConstructible<value_type>
+  value_type pop() {
+    value_type result;
+    // TODO(alasdair): Add a proper implementation of this.
+    while (!try_pop(result)) {
+      this_thread::sleep_for(10);
+    }
+    return result;
+  }
+
+  // requires Swappable<Cont>
+  void swap(concurrent_priority_queue& other) {
+    using std::swap;
+    swap(cont_, other.cont_);
+    swap(less_, other.less_);
+  }
+
+ private:
+  void make_heap() {
+    std::make_heap(cont_.begin(), cont_.end(), less_);
+  }
+
+  Less less_;
+  Cont cont_;
+};
+
+#endif  // STD_CONCURRENT_PRIORITY_QUEUE_
---
+++ testing/concurrent_priority_queue_test.cc
@@ -0,0 +1,320 @@
+// Copyright 2009 Google Inc. All Rights Reserved.
+
+// This file is a rudimentary test of the concurrent_priority_queue class
+#include "concurrent_priority_queue.h"
+
+#include "gmock/gmock.h"
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <vector>
+
+using namespace std;
+using testing::_;
+using testing::Invoke;
+using testing::InSequence;
+
+// Iterator used with MyContainer
+// TODO(alasdair): Verify if we need this. Keeping it for now until
+// all functionality and multithreaded tests are complete.
+class MyIterator : public iterator<random_access_iterator_tag, string> {
+ public:
+  MyIterator() : ptr_(NULL) {}
+  explicit MyIterator(string* x) : ptr_(x) {}
+  MyIterator(const MyIterator& other) : ptr_(other.ptr_) {}
+
+  MyIterator& operator++() {
+    ++ptr_;
+    return *this;
+  }
+
+  MyIterator& operator++(int) {
+    ptr_++;
+    return *this;
+  }
+
+  MyIterator& operator--() {
+    --ptr_;
+    return *this;
+  }
+
+  MyIterator& operator--(int) {
+    ptr_--;
+    return *this;
+  }
+
+  MyIterator& operator+=(int inc) {
+    ptr_ += inc;
+    return *this;
+  }
+
+  MyIterator& operator-=(int dec) {
+    ptr_ -= dec;
+    return *this;
+  }
+
+  bool operator==(const MyIterator& rhs) const {
+    return ptr_ == rhs.ptr_;
+  }
+
+  bool operator<(const MyIterator& rhs) const {
+    return ptr_ < rhs.ptr_;
+  }
+
+  bool operator>(const MyIterator& rhs) const {
+    return ptr_ > rhs.ptr_;
+  }
+
+  bool operator!=(const MyIterator& rhs) const {
+    return ptr_ !=rhs.ptr_;
+  }
+
+  MyIterator operator+(int val) {
+    return MyIterator(ptr_ + val);
+  }
+
+  MyIterator operator-(int val) {
+    return MyIterator(ptr_ - val);
+  }
+
+  int operator-(const MyIterator& rhs) const {
+    return ptr_ - rhs.ptr_;
+  }
+  string& operator*() {return *ptr_;}
+ private:
+  string* ptr_;
+};
+
+// Container type used with a concurrent_priority_queue
+class MyContainer {
+ public:
+  MyContainer() : size_(0) {
+  }
+
+  MyIterator begin() {
+    MyIterator it(&elems_[0]);
+    return it;
+  }
+
+  MyIterator end() {
+    MyIterator it(&elems_[size_]);
+    return it;
+  }
+
+  void push_back(string elem) {
+    elems_[size_++] = elem;
+  }
+
+  void pop_back() {
+    size_--;
+  }
+
+  const string& back() const {
+    return elems_[size_ - 1];
+  }
+
+  MyIterator erase(MyIterator pos) {
+    int position = pos - begin();
+    for (int i = position; i < size_; i++) {
+      elems_[i] = elems_[i+1];
+    }
+    size_--;
+    return MyIterator(&elems_[position]);
+  }
+
+  const string& operator[](int i) const {
+    return elems_[i];
+  }
+
+  int size() const {
+    return size_;
+  }
+
+  bool empty() const {
+    return size_ == 0;
+  }
+
+ private:
+  string elems_[100];
+  int size_;
+};
+
+// Switchable comparison function
+class MyCompare : public binary_function <string, string, bool> {
+ public:
+  explicit MyCompare(bool reverse) : reverse_(reverse) {
+  }
+
+  bool operator() (const string& x, const string& y) const {
+    bool result = reverse_ ? x > y : x < y;
+    return result;
+  }
+
+ private:
+  bool reverse_;
+};
+
+class PriorityQueueTest : public testing::Test {
+ protected:
+  vector<string> create_values() {
+    vector<string> values;
+    values.push_back("C");
+    values.push_back("A");
+    values.push_back("D");
+    values.push_back("B");
+    return values;
+  }
+
+  // Verifies that the given queue contains the same elements as the
+  // expected container. This method pops values off the queue, and
+  // expects that the order of elements in the container matches the
+  // queue. (The last element in the container will be the first one
+  // popped.)
+  template <typename Queue, typename Container>
+  void validate_queue(Queue& queue, const Container& expected) {
+    size_t expected_size = expected.size();
+    ASSERT_EQ(expected_size, queue.size());
+    for (size_t i = expected.size(); i > 0; i--) {
+      string value;
+      ASSERT_TRUE(queue.try_pop(value));
+      EXPECT_EQ(expected[i - 1], value);
+    }
+  }
+};
+
+// Verify that a concurrent_priority_queue can be created from a
+// vector of values.
+TEST_F(PriorityQueueTest, Simple) {
+  vector<string> values = create_values();
+  concurrent_priority_queue<string> queue(values);
+  sort(values.begin(), values.end());
+  validate_queue(queue, values);
+}
+
+// Verify that a concurrent_priority_queue can be created from a
+// vector of values and a custom comparator.
+TEST_F(PriorityQueueTest, CustomComparator) {
+  MyCompare cmp(true);
+  vector<string> values = create_values();
+  concurrent_priority_queue<string, vector<string>, MyCompare> queue(
+      cmp, values);
+  sort(values.begin(), values.end());
+  reverse(values.begin(), values.end());
+  validate_queue(queue, values);
+}
+
+// Verify the a concurrent_priority_queue can be created from another
+// queue.
+TEST_F(PriorityQueueTest, CopyConstructor) {
+  vector<string> values = create_values();
+  concurrent_priority_queue<string> queue(values);
+  concurrent_priority_queue<string> new_queue(queue);
+  // Pop an element off the original queue. Should not affect the new
+  // queue
+  string dummy;
+  ASSERT_TRUE(queue.try_pop(dummy));
+  sort(values.begin(), values.end());
+  validate_queue(new_queue, values);
+}
+
+// Verify the a concurrent_priority_queue can be created from an
+// iterator.
+TEST_F(PriorityQueueTest, CreateFromIterator) {
+  vector<string> values = create_values();
+  concurrent_priority_queue<string> queue(values.begin(), values.end());
+  sort(values.begin(), values.end());
+  validate_queue(queue, values);
+}
+
+// Verify that a concurrent_priority_queue can be created from an
+// iterator, a custom comparator and a container.
+TEST_F(PriorityQueueTest, IteratorAndCustomComparator) {
+  MyCompare cmp(true);
+  vector<string> base;
+  base.push_back("ZZZ");
+  vector<string> values = create_values();
+
+  concurrent_priority_queue<string, vector<string>, MyCompare> queue(
+      values.begin(), values.end(), cmp, base);
+  base.insert(base.end(), values.begin(), values.end());
+  sort(base.begin(), base.end());
+  reverse(base.begin(), base.end());
+  validate_queue(queue, base);
+}
+
+// Verify the a concurrent_priority_queue can be created from a
+// non-vector queue.
+TEST_F(PriorityQueueTest, CreateFromCustomQueue) {
+  MyContainer my_values;
+  my_values.push_back("A");
+  my_values.push_back("X");
+  my_values.push_back("B");
+  concurrent_priority_queue<string, MyContainer, less<string> >
+      myqueue(less<string>(), my_values);
+  sort(my_values.begin(), my_values.end());
+  validate_queue(myqueue, my_values);
+}
+
+// Verify the a concurrent_priority_queue can be created from a
+// non-vector queue and a custom comparison function.
+TEST_F(PriorityQueueTest, CreateFromCustomQueueAndComparator) {
+  MyContainer my_values;
+  my_values.push_back("A");
+  my_values.push_back("X");
+  my_values.push_back("B");
+  MyCompare cmp(true);
+  concurrent_priority_queue<string, MyContainer, MyCompare> myqueue(
+      cmp, my_values);
+
+  sort(my_values.begin(), my_values.end());
+  reverse(my_values.begin(), my_values.end());
+  validate_queue(myqueue, my_values);
+}
+
+// Verify the a concurrent_priority_queue can be created from another
+// queue that uses a custom comparator.
+TEST_F(PriorityQueueTest, CopyConstructorWithComparator) {
+  vector<string> values = create_values();
+  MyCompare cmp(true);
+  concurrent_priority_queue<string, vector<string>, MyCompare> myqueue(
+      cmp, values);
+  concurrent_priority_queue<string, vector<string>, MyCompare> new_queue(
+      myqueue);
+  // Pop an element off the original queue. Should not affect the new queue
+  string dummy;
+  ASSERT_TRUE(myqueue.try_pop(dummy));
+
+  // Add an element to the new queue. The reverse comparator should
+  // put it at the beginning.
+  new_queue.push("ZZZ");
+  sort(values.begin(), values.end());
+  reverse(values.begin(), values.end());
+  values.insert(values.begin(), "ZZZ");
+  validate_queue(new_queue, values);
+}
+
+// Verify the a concurrent_priority_queue can be re-ordered on update()
+// uses a custom comparator.
+TEST_F(PriorityQueueTest, Update) {
+  vector<string> input;
+  input.push_back("C");
+  input.push_back("B");
+  input.push_back("A");
+  input.push_back("Z");
+  MyCompare cmp(false);
+  concurrent_priority_queue<string, vector<string>, MyCompare> myqueue(
+      cmp, input);
+  string head;
+  ASSERT_TRUE(myqueue.try_pop(head));
+  ASSERT_EQ(head, "Z");
+
+  // update the queue with a reversed comparator. Order should change.
+  myqueue.update(MyCompare(true));
+  ASSERT_TRUE(myqueue.try_pop(head));
+  ASSERT_EQ(head, "A");
+}
+
+// TODO(alasdair): Add multithreaded tests once we have a wait/notify mechanism
+
+// TODO(alasdair): Add additional tests for the methods currently
+// commented out because of C++0x incompatibility.
