90dab08b28b4c706801e437aaf7bddd5243c426a
lawrence.crowl@gmail.com <lawrence.crowl@gmail.com@003f9db4-d5f8-dca4-bccd-e9ef3f936871>
2012-05-25 18:40:46 +0000

    Add pass/fail files.

--- util/exetest.sh
+++ util/exetest.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+rm -f $1.pass $1.fail
 if ./$1.exe >/tmp/$1.run 2>&1 
 then
-	mv /tmp/$1.run $1.run
+	mv /tmp/$1.run $1.pass
 	exit 0
 else
 	cat /tmp/$1.run
-	rm /tmp/$1.run
+	mv /tmp/$1.fail
 	exit 1
 fi
